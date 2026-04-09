1d42178d60c83ce8f6950ecdce4a3b29685e1eee
thienkim26 <2396044@cmontmorency.qc.ca>
2026-04-09 17:38:31 -0400

    Les routes des users, qui ressemble les requetes dans Postman

---
+++ backend/routes/users-routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import usersController from "../controllers/users-controller.js";
+
+const router = express.Router();
+
+//Route à l'inscription
+router.post('/register', usersController.registerUser);
+//Route au connexion
+router.post('login', usersController.login);
+
+export default router;
